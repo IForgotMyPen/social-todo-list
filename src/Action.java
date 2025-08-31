7bd22f5359b5be94381571f03fe8a17108dda89d
unknown <brendanbirsc@gmail.com>
2025-08-31 15:01:41 -0500

    Added overloaded constructor for when an action has already been stored in the data files

--- src/Action.java
+++ src/Action.java
@@ -6,27 +6,36 @@ public class Action {
   // FIELDS ----------------------------------------------------------------------------------------
 
   User user;
-  String description;
   String dateAdded;
   String dateDue;
   String dateCompleted;
   boolean isComplete;
+  String description;
 
   // CONSTRUCTOR -----------------------------------------------------------------------------------
 
-  public Action(User user, String description, String dateDue) {
+  public Action(User user, String dateDue, String description) {
     LocalDateTime currentDateTime = LocalDateTime.now();
-    DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+    DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy/HH:mm:ss");
 
-    // The user who created the action
     this.user = user;
-    this.description = description;
     this.dateAdded = currentDateTime.format(dateTimeFormat);
     // Want to make sure the date is in the correct format first, also want to ask if the user wants
     // to set a specific time of day
     this.dateDue = dateDue;
-    this.dateCompleted = null;
+    this.dateCompleted = "incomplete";
     this.isComplete = false;
+    this.description = description;
+  }
+
+  public Action(
+      User user, String dateAdded, String dateDue, String dateCompleted, String description) {
+    this.user = user;
+    this.dateAdded = dateAdded;
+    this.dateDue = dateDue;
+    this.dateCompleted = dateCompleted;
+    this.isComplete = !this.dateCompleted.equals("incomplete");
+    this.description = description;
   }
 
   // GETTERS, SETTERS, & TOGGLES -------------------------------------------------------------------
